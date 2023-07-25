4d90f147a53b75461b675826394df8f221b281b2
Nathnael Dereje <nathandere1357@gmail.com>
2023-07-25 03:52:51 +0300

    Add a created_at attribute to the Course

--- Course.py
+++ Course.py
@@ -1,12 +1,16 @@
+from datetime import datetime
+
+
 class Course:
     def __init__(
         self, course_name: str, course_number: int, credit_hours: float, **kwargs
     ):
-        self.course_name = course_name
-        self.course_number = course_number
+        self.course_name: str = course_name
+        self.course_number: int = course_number
         self.credit_hours = credit_hours
         self.hours_completed = kwargs.get("hours_completed", 0.0)
         self.is_active = kwargs.get("is_active", True)
+        self.created_at: str = kwargs.get("created_at", datetime.now().isoformat())
 
     def is_complete(self) -> bool:
         return self.hours_completed >= self.credit_hours
@@ -18,8 +22,8 @@ class Course:
             "credit_hours": self.credit_hours,
             "hours_completed": self.hours_completed,
             "is_active": self.is_active,
+            "created_at": self.created_at,
         }
-    
 
     def update(self, **kwargs):
         self.course_name = kwargs.get("course_name", self.course_name)
@@ -27,6 +31,8 @@ class Course:
         self.credit_hours = kwargs.get("credit_hours", self.credit_hours)
         self.hours_completed = kwargs.get("hours_completed", self.hours_completed)
         self.is_active = kwargs.get("is_active", self.is_active)
+        # Here it doesn't allow the user to change the created_at date 
+        # because it is not a good practice to change the created_at date.
 
     @staticmethod
     def fromJson(json):
@@ -36,6 +42,7 @@ class Course:
             json["credit_hours"],
             hours_completed=json["hours_completed"],
             is_active=json["is_active"],
+            created_at=json["created_at"],
         )
         return course
 
