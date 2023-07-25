b28138ec8a75a00379ec8bfcf55aef024accef97
Nathnael Dereje <nathandere1357@gmail.com>
2023-07-25 03:57:47 +0300

    Modify CourseList class
    Add check for update course if name is updated. This will cause a trouble because course_name is used as a key for the self.courses. If course_name is edited the prev course is deleted from the dictionary and a new key value pair with the new course name as a key and the updated course object as a value will be added to the dictionary.

--- CourseList.py
+++ CourseList.py
@@ -1,6 +1,4 @@
 from Course import *
-from typing import Dict, List
-
 import json
 
 
@@ -8,19 +6,28 @@ class CourseList:
     """This class is used to store the list of courses that a student is taking."""
 
     def __init__(self):
-        self.courses : Dict[str, Course]= {}
+        self.courses: dict[str, Course] = self.__read_file()  # course_name: Course
 
     def create_course(self, course: Course):
         """This method is used to add a course to the list."""
-        self.courses[course] = course
+        if course.course_name in self.courses:
+            print("A course with the same name already exists.")
+            return
+        self.courses[course.course_name] = course
         self.__save_to_file()
+
+    def get_courses(self):
+        """This method is used to get a course from the list."""
+        return self.courses.values()
 
     def delete_course(self, course_name: str, **kwarg):
         """This method is used to delete a course from the list."""
         if course_name in self.courses:
             if kwarg.get("hard", False):
+                # Hard delete
                 del self.courses[course_name]
             else:
+                # Soft delete
                 self.courses[course_name].is_active = False
             self.__save_to_file()
         else:
@@ -30,20 +37,23 @@ class CourseList:
         """This method is used to update a course in the list."""
         if course_name in self.courses:
             self.courses[course_name].update(**kwarg)
+            updated_course = self.courses[course_name]
+            # check if the course is renamed
+            if course_name != updated_course.course_name:
+                # delete the old course
+                del self.courses[course_name]
+                # add the new course
+                self.courses[updated_course.course_name] = updated_course
+
             self.__save_to_file()
         else:
             print("No such course exists in the list.")
 
-
-    def get_courses(self) -> List[Course]:
-        """This method is used to get a course from the list."""
-        return list(self.courses.values())
-
     def __read_file(self) -> dict[str, Course]:
         """This method is used to read the courses from the saved file."""
         try:
-            with open("courses.json", "r") as f:
-                courses = json.load(f)
+            with open("courses.json", "r") as file:
+                courses = json.load(file)
                 return {course: Course.fromJson(courses[course]) for course in courses}
         except:
             print("Error reading courses from file.")
@@ -52,10 +62,10 @@ class CourseList:
     def __save_to_file(self):
         """This method is used to save the courses to the file."""
         try:
-            with open("courses.json", "w") as f:
+            with open("courses.json", "w") as file:
                 courses = {
                     course: self.courses[course].toJson() for course in self.courses
                 }
-                json.dump(courses, f)
+                json.dump(courses, file)
         except:
             print("Error saving courses to file.")
