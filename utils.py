bdc127597b7d996553a00718e7498f994f848114
Nathnael Dereje <nathandere1357@gmail.com>
2023-07-27 01:53:27 +0300

    Add helper function that generates a select query string
    Add a string generator for executing a select command on sqlite

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-import sys
-
 # Utility methods
+
+
 def get_input():
     """
     Get input from the user.
@@ -10,9 +10,35 @@ def get_input():
 
 
 class any:
-    """
-    A class that represents any type.
-    """
+    """A class that represents any type."""
 
     def __eq__(self, _):
         return True
+
+
+def is_any(value):
+    """Check if a value is of type any."""
+    return isinstance(value, any)
+
+
+# SQL QUERY STRING GENERATORS #
+def select_query_string(
+    table_name: str,
+    active_status: bool | any = any(),
+    completed_status: bool | any = any(),
+) -> str:
+    """
+    Generate a select query string for task selection.
+    """
+
+    if is_any(active_status) and is_any(completed_status):
+        return f"SELECT * FROM {table_name}"
+    
+    elif not is_any(active_status) and is_any(completed_status):
+        return f"SELECT * FROM {table_name} WHERE task_is_active = {1 if active_status else 0}"
+    
+    elif is_any(active_status) and not is_any(completed_status):
+        return f"SELECT * FROM {table_name} WHERE task_is_completed = {1 if completed_status else 0}"
+    
+    else:
+        return f"SELECT * FROM {table_name} WHERE task_is_active = {1 if active_status else 0} AND task_is_completed = {1 if completed_status else 0}"
